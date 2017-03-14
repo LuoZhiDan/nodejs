dddb7af5d27fbd4fa76d2d92b843331b5bad877a
LuoZhiDan <835825423@qq.com>
2017-03-14 21:41:50 +0800

    'express'

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-/**
- * Created by zdluoa on 2017/3/13.
- */
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.end('hello world!');
-}).listen(8888);
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-console.log('server running!  98888');
+app.get('/index', function (req, res) {
+    res.send('欢迎进入学生管理系统!');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
